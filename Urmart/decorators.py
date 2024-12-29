5b928a0cd507cd9eaa3a964913c5b86186d54ddf
KK-Huang86 <iankk0806@gmail.com>
2024-12-29 15:25:57 +0800

    refactor: member and product has vip tag then can buy vip product

--- Urmart/decorators.py
+++ Urmart/decorators.py
@@ -72,26 +72,78 @@ def check_vip_identity(view_func):
         if not isinstance(request, Request):
             return Response({"error": "無效的請求"}, status=status.HTTP_400_BAD_REQUEST) #確保 request是否為DRF中Request類型
 
-        data = request.data
-        product_id = data.get("product")
+        # serializer = view_instance.get_serializer(data=request.data)
+        # serializer.is_valid(raise_exception=True)
+        # validated_data = serializer.validated_data
 
-        if product_id:
-            try:
-                product = Product.objects.get(id=product_id)
-                if product.is_vip:
-                    return Response(
-                        {"error": "VIP 商品無法購買"},
-                        status=status.HTTP_400_BAD_REQUEST,
+        data = request.data
+        print(f'Request Data: {request.data}')
+        product_id = data.get("product")
+        member_id = data.get("member_id")
+
+        # product_id = validated_data.get("product")
+        # member_id = validated_data.get("member_id")
+
+        print(f'-----{member_id}-----')
+
+        if not product_id or not member_id:
+
+            return Response(
+                {"error": "商品或會員資訊缺失"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            product = Product.objects.get(id=product_id)
+            member = Member.objects.get(id=member_id)
+            if  product.is_vip and not member.is_vip:
+
+             return Response(
+                    {"error": "VIP 商品無法購買"},
+                     status=status.HTTP_400_BAD_REQUEST,
                     )
-            except Product.DoesNotExist:
+        except Product.DoesNotExist:
                 return Response(
                     {"error": "商品不存在"}, status=status.HTTP_404_NOT_FOUND
                 )
+        except Member.DoesNotExist:
+            return Response(
+                {"error": "會員不存在"},
+                status=status.HTTP_404_NOT_FOUND
+            )
 
         return view_func(view_instance, request, *args, **kwargs)
 
     return wrapper
 
+
+def check_stock(view_func):
+    @wraps(view_func)
+    def wrapper(view_instance, request, *args, **kwargs):
+        if not isinstance(request, Request):
+            return Response({"error": "無效的請求"}, status=400)
+
+        data = request.data
+        product_id = data.get("product")
+        qty = data.get("qty")
+
+        if view_instance.action == "destroy":
+            return view_func(view_instance, request, *args, **kwargs)
+
+        if not product_id or qty is None:
+            return Response(
+                {"error": "缺少商品或數量資訊"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            qty = int(qty)
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return Response("商品不存在", status=404)
+
+        if product.stock_pcs < qty:
+            return Response("商品庫存不夠，無法購買", status=400)
+        return view_func(view_instance, request, *args, **kwargs)
+
+    return wrapper
 
 # def check_stock(view_func):
 #     @wraps(view_func)
@@ -123,31 +175,4 @@ def check_vip_identity(view_func):
 #     return wrapper
 
 
-def check_stock(view_func):
-    @wraps(view_func)
-    def wrapper(view_instance, request, *args, **kwargs):
-        if not isinstance(request, Request):
-            return Response({"error": "無效的請求"}, status=400)
 
-        data = request.data
-        product_id = data.get("product")
-        qty = data.get("qty")
-
-        if view_instance.action == "destroy":
-            return view_func(view_instance, request, *args, **kwargs)
-
-        if not product_id or qty is None:
-            return Response(
-                {"error": "缺少商品或數量資訊"}, status=status.HTTP_400_BAD_REQUEST
-            )
-        try:
-            qty = int(qty)
-            product = Product.objects.get(id=product_id)
-        except Product.DoesNotExist:
-            return Response("商品不存在", status=404)
-
-        if product.stock_pcs < qty:
-            return Response("商品庫存不夠，無法購買", status=400)
-        return view_func(view_instance, request, *args, **kwargs)
-
-    return wrapper
--- Urmart/models.py
+++ Urmart/models.py
@@ -1,45 +1,46 @@
 from datetime import datetime
 
 from django.db import models
+from django.core.exceptions import ValidationError
+
 
 # Create your models here.
 
 
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    stock_pcs = models.PositiveIntegerField(default=0, null=False)
-    price = models.DecimalField(max_digits=10, decimal_places=0, null=False)
+    stock_pcs = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=0,default=0)
     shop_choice = ((1, "um"), (2, "ms"), (3, "ps"))
     shop_id = models.PositiveIntegerField(choices=shop_choice, default=1)
-    is_vip = models.BooleanField(default=False, null=False)
+    is_vip = models.BooleanField(default=False)
 
     def __str__(self):
         return f"商品id:{self.id} 庫存:{self.stock_pcs} 價格:{self.price}"
 
 
 class Member(models.Model):
-    id = models.AutoField(primary_key=True)
-    member_name = models.CharField(max_length=100, null=False, default="")
+    member_name = models.CharField(max_length=100, default="")
+    is_vip=models.BooleanField(default=False)
 
     def __str__(self):
         return f"會員號碼 : {self.id} 名字: {self.member_name}"
 
 
 class Order(models.Model):
-    id = models.AutoField(primary_key=True)
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
         related_name="orders",
-        null=False,
         help_text="購買產品",
+        null=True,
+        blank=True,
     )
-    qty = models.PositiveIntegerField(default=0, null=False, help_text="購買數量")
+    qty = models.PositiveIntegerField(default=0, help_text="購買數量")
     price = models.DecimalField(
-        max_digits=10, decimal_places=0, null=False, help_text="商品單價"
+        max_digits=10, decimal_places=0, help_text="商品單價",default=0
     )
     total_price = models.DecimalField(
-        max_digits=10, decimal_places=0, null=False, default=0, help_text="訂單總額"
+        max_digits=10, decimal_places=0, default=0, help_text="訂單總額"
     )
     shop_choice = ((1, "um"), (2, "ms"), (3, "ps"))
     shop_id = models.PositiveIntegerField(
@@ -49,21 +50,27 @@ class Order(models.Model):
         Member,
         on_delete=models.CASCADE,
         related_name="orders",
-        null=True,
         help_text="購買成員",
+        null=True,
+        blank=True,
     )
-    created_at = models.DateTimeField(default=datetime.now, null=False)
+    created_at = models.DateTimeField(default=datetime.now)
+
+    def clean(self):
+        if not self.product:
+            raise ValidationError("需輸入購買的商品")
+        if not self.member:
+            raise ValidationError("需輸入購買的會員")
 
     def __str__(self):
         return f"Order:{self.id} (Product:{self.product.id}, Qty:{self.qty})"
 
 
 class ShopSalesStats(models.Model):
-    id = models.AutoField(primary_key=True)
     shop_choice = ((1, "um"), (2, "ms"), (3, "ps"))
     shop_id = models.PositiveIntegerField(choices=shop_choice, default=1)
     total_sales_amount = models.DecimalField(
-        max_digits=12, decimal_places=2, null=False, default=0
+        max_digits=12, decimal_places=2, default=0
     )
     total_qty = models.PositiveIntegerField(default=0)
     total_orders = models.PositiveIntegerField(default=0)
